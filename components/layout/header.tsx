f02e6cbaac170817fcee8a6ed1aa2c696eef7acc
Elijah <elijah.pyatihatko@gmail.com>
2023-04-06 21:36:35 +0300

    base layout

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
-type Props = {};
+type HeaderProps = {};
 
-const Header = (props: Props) => {
+const Header: React.FC<HeaderProps> = () => {
   return (
-    <header className="h-16 border-b border-green-500">
+    <header className="h-16 border-b">
       <div className="h-full flex justify-between items-center px-4">
         <h1 className=" font-bold text-2xl">ReactJobs</h1>
-        <div>nav items</div>
-        <div>user</div>
+        <button className="py-1 px-4 border border-green-300 rounded-full bg-green-200 text-green-800">
+          Sign In
+        </button>
       </div>
     </header>
   );
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import Header from "./header";
+import Link from "next/link";
 
 type LayoutProps = {
   children: ReactNode;
@@ -7,10 +8,33 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-[100vh]">
+    <div className="min-h-[100vh] flex flex-col">
       <Header />
-      <main>{children}</main>
-      <footer>footer</footer>
+      <div className="flex flex-1">
+        <div className="w-[15vw] border-r">
+          <div className="flex flex-col mt-16 px-4 gap-4">
+            <Link
+              className="font-bold text-xl hover:bg-slate-200 p-1 rounded"
+              href="/jobs"
+            >
+              Jobs
+            </Link>
+            <Link
+              className="font-bold text-xl hover:bg-slate-200 p-1 rounded"
+              href="/companies"
+            >
+              Companies
+            </Link>
+            <Link
+              className="font-bold text-xl hover:bg-slate-200 p-1 rounded"
+              href="/companies"
+            >
+              Dashboard
+            </Link>
+          </div>
+        </div>
+        <main className="flex-1">{children}</main>
+      </div>
     </div>
   );
 };
